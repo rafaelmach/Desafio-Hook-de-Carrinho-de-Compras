6f8c27cfdb7b3943229f10b817d89c1411be6b34
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2022-04-10 11:41:56 -0300

    Lógica de adicionar produtos no hook useCart

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,56 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { MdAddShoppingCart } from 'react-icons/md';
+import React, { useState, useEffect } from "react"
+import { MdAddShoppingCart } from "react-icons/md"
 
-import { ProductList } from './styles';
-import { api } from '../../services/api';
-import { formatPrice } from '../../util/format';
-import { useCart } from '../../hooks/useCart';
+import { ProductList } from "./styles"
+import { api } from "../../services/api"
+import { formatPrice } from "../../util/format"
+import { useCart } from "../../hooks/useCart"
 
 interface Product {
-  id: number;
-  title: string;
-  price: number;
-  image: string;
+  id: number
+  title: string
+  price: number
+  image: string
 }
 
 interface ProductFormatted extends Product {
-  priceFormatted: string;
+  priceFormatted: string
 }
 
 interface CartItemsAmount {
-  [key: number]: number;
+  [key: number]: number
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([])
+  const { addProduct, cart } = useCart()
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    TODO
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      TODO
     }
 
-    loadProducts();
-  }, []);
+    loadProducts()
+  }, [])
 
   function handleAddProduct(id: number) {
-    // TODO
+    TODO
   }
 
   return (
     <ProductList>
       <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
+        <img
+          src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg"
+          alt="Tênis de Caminhada Leve Confortável"
+        />
         <strong>Tênis de Caminhada Leve Confortável</strong>
         <span>R$ 179,90</span>
         <button
           type="button"
           data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
+          // onClick={() => handleAddProduct(product.id)}
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
@@ -61,7 +64,7 @@ const Home = (): JSX.Element => {
         </button>
       </li>
     </ProductList>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
